8c6bcff786e5fbf7531f1cac06be35f19a7f7f53
Fisayo Watti <wf.watti@gmail.com>
2021-02-11 16:40:25 +0100

    initial commit
    added reat navigation to App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+export default function App() {
+  return <NavigationContainer></NavigationContainer>;
+}
+
+const styles = StyleSheet.create({});
