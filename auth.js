f9d7d630487a14f114c33a2fe6bbf193989cbdcf
Fisayo Watti <wf.watti@gmail.com>
2021-02-12 18:45:39 +0100

    Add app auth module

---
+++ auth.js
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect, useState } from "react";
+import { auth as firebaseAuth } from "./firebase";
+
+export const AuthContext = React.createContext({ loggedIn: false });
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const useAuthState = () => {
+  const [authState, setAuthState] = useState({ loading: true, auth: {} });
+
+  useEffect(() => {
+    return firebaseAuth.onAuthStateChanged((firebaseUser) => {
+      setAuthState({
+        loading: false,
+        auth: { loggedIn: Boolean(firebaseUser), userId: firebaseUser?.uid },
+      });
+    });
+  }, []);
+
+  return authState;
+};
