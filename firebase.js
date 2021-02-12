4344a7eab4da49dce4d9eb8df8c53e3b3006fc9d
Fisayo Watti <wf.watti@gmail.com>
2021-02-12 18:46:00 +0100

    Add firestore to firebase module

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-// import 'firebase/firestore';
+import "firebase/firestore";
 // import 'firebase/storage';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -14,8 +14,9 @@ const firebaseConfig = {
   measurementId: "G-4HHTBGED76",
 };
 
+//check if !firebase.apps.length before initializeApp
 const app = firebase.initializeApp(firebaseConfig);
 
 export const auth = app.auth();
-// export const firestore = app.firestore();
+export const firestore = app.firestore();
 // export const storage = app.storage();
