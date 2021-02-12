6757517514b26bf05f375f9d99c00c0d0a99166f
Fisayo Watti <wf.watti@gmail.com>
2021-02-12 18:49:25 +0100

    Implement Kent C Dodds' react authentication flow

--- App.js
+++ App.js
@@ -1,10 +1,44 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+// import { StatusBar } from "expo-status-bar";
+import React, { Suspense } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { useAuthState } from "./auth";
+import AppProviders from "./components/app/AppProviders";
+
+// const Stack = createStackNavigator();
+
+const Loading = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Loading</Text>
+    </View>
+  );
+};
+
+const PreApp = () => {
+  const { loading, auth } = useAuthState();
+
+  const AuthenticatedApp = React.lazy(() =>
+    import("./components/app/AuthenticatedApp")
+  );
+  const UnauthenticatedApp = React.lazy(() =>
+    import("./components/app/UnauthenticatedApp")
+  );
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return auth.loggedIn ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+};
 
 export default function App() {
-  return <NavigationContainer></NavigationContainer>;
+  return (
+    <AppProviders>
+      <Suspense fallback={<Loading />}>
+        <PreApp />
+      </Suspense>
+    </AppProviders>
+  );
 }
 
 const styles = StyleSheet.create({});
