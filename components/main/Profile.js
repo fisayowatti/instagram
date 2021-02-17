ac898a90e6940efc56110fcb9be943c1584da17e
Fisayo Watti <wf.watti@gmail.com>
2021-02-17 14:01:29 +0100

    create and use new redux action

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,11 +1,33 @@
 import React from "react";
 
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { connect } from "react-redux";
 
-export default function Profile() {
+function Profile(props) {
+  const { posts, currentUser } = props;
+  console.log(posts);
   return (
-    <View>
-      <Text>Profile Screen</Text>
+    <View style={styles.container}>
+      {/* <Image
+        source={{uri: }}
+      /> */}
+      <Text>Policeeee</Text>
+      <Text>Policeeee</Text>
+      <Text>{posts[0]?.id}</Text>
+      <Text>{currentUser.name}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+  },
+});
+
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+  posts: store.userState.posts,
+});
+
+export default connect(mapStateToProps, null)(Profile);
--- redux/actions/index.js
+++ redux/actions/index.js
@@ -1,5 +1,5 @@
 import { firestore } from "../../firebase";
-import { USER_STATE_CHANGE } from "../constants/index";
+import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE } from "../constants/index";
 // import firebase from "firebase";
 // require("firebase/firestore");
 
@@ -12,9 +12,52 @@ export function fetchUser(userId) {
       .then((snapshot) => {
         if (snapshot.exists) {
           dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() });
+          console.log(snapshot.data());
         } else {
           console.log("does not exist");
         }
       });
   };
 }
+
+export function fetchUserPosts(userId) {
+  return (dispatch) => {
+    console.log("basic", userId);
+    firestore
+      .collection("posts")
+      .doc(userId)
+      .collection("userPosts")
+      .orderBy("creation", "asc")
+      .get()
+      .then((snapshot) => {
+        console.log("simole");
+        let posts = snapshot.docs.map((doc) => {
+          const id = doc.id;
+          const data = doc.data();
+          return { id, ...data };
+        });
+        dispatch({ type: USER_POSTS_STATE_CHANGE, posts });
+      });
+  };
+}
+
+// export function fetchUserPosts(userId) {
+//   return (dispatch) => {
+//     console.log("basic", userId);
+//     firestore
+//       .collection("posts")
+//       .doc(userId)
+//       .collection("userPosts")
+//       .orderBy("creation", "asc")
+//       .get()
+//       .then((snapshot) => {
+//         console.log("simole");
+//         let posts = snapshot.docs.map((doc) => {
+//           const id = doc.id;
+//           const data = doc.data();
+//           return { id, ...data };
+//         });
+//         dispatch({ type: USER_POSTS_STATE_CHANGE, posts });
+//       });
+//   };
+// }
