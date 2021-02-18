fc2711e4b669cc464df5fac735e9e4d8b160548d
Fisayo Watti <wf.watti@gmail.com>
2021-02-18 11:28:52 +0100

    Set up ProfileScreen to display user's uploaded pictures

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
 import { connect } from "react-redux";
 
 function Profile(props) {
@@ -8,13 +8,19 @@ function Profile(props) {
   console.log(posts);
   return (
     <View style={styles.container}>
-      {/* <Image
-        source={{uri: }}
-      /> */}
-      <Text>Policeeee</Text>
-      <Text>Policeeee</Text>
-      <Text>{posts[0]?.id}</Text>
-      <Text>{currentUser.name}</Text>
+      <View style={styles.galleryContainer}>
+        <FlatList
+          keyExtractor={(item) => item.id}
+          numColumns={3}
+          data={posts}
+          horizontal={false}
+          renderItem={({ item }) => (
+            <View style={styles.imgContainer}>
+              <Image source={{ uri: item.downloadURL }} style={styles.img} />
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -22,6 +28,19 @@ function Profile(props) {
 const styles = StyleSheet.create({
   container: {
     marginTop: 40,
+    paddingLeft: 16,
+    paddingRight: 16,
+    flex: 1,
+  },
+  galleryContainer: {
+    flex: 1,
+  },
+  imgContainer: {
+    flex: 1 / 3,
+  },
+  img: {
+    flex: 1,
+    aspectRatio: 1 / 1,
   },
 });
 
