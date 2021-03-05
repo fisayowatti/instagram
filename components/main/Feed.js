189350a958499b64ab0aa367a8a8fbf0b906138f
Fisayo Watti <wf.watti@gmail.com>
2021-03-05 18:31:00 +0100

    Add like and dislike functionality

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -1,15 +1,73 @@
 import React, { useEffect, useState } from "react";
-
-import { View, Text, FlatList, Image, Dimensions } from "react-native";
+import { View, Text, FlatList, Image, StyleSheet } from "react-native";
 import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import { useAuth } from "../../auth";
+import { firestore } from "../../firebase";
 
-//todo - sort userFollowingPosts according to creation date
+//something weird is happening where trying to update a value in a post is resulting in duplicate posts
 
-function Feed({ userFollowingPosts, navigation }) {
+function Feed({ userFollowingPosts, navigation, likedPosts }) {
+  const { userId } = useAuth();
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const sortedPosts = userFollowingPosts.sort(
+      (a, b) => a.creation - b.creation
+    );
+    setPosts(sortedPosts);
+  }, [userFollowingPosts]);
+
   if (userFollowingPosts.length < 1) {
-    return <View />;
+    return (
+      <View style={styles.emptyContainer}>
+        <Text>There's no post on your feed yet</Text>
+      </View>
+    );
   }
+
+  const increaseLikesCount = (postId, creatorId) => {
+    return firestore
+      .collection("posts")
+      .doc(creatorId)
+      .collection("userPosts")
+      .doc(postId)
+      .update({
+        likesCount: posts.find((post) => post.id === postId)["likesCount"] + 1,
+      });
+  };
+  const decreaseLikesCount = (postId, creatorId) => {
+    return firestore
+      .collection("posts")
+      .doc(creatorId)
+      .collection("userPosts")
+      .doc(postId)
+      .update({
+        likesCount: posts.find((post) => post.id === postId)["likesCount"] - 1,
+      });
+  };
+
+  const onLikePress = (postId, creatorId) => {
+    firestore
+      .collection("likes")
+      .doc(userId)
+      .collection("userLikes")
+      .doc(postId)
+      .set({})
+      .then(() => increaseLikesCount(postId, creatorId));
+  };
+
+  const onDislikePress = (postId, creatorId) => {
+    firestore
+      .collection("likes")
+      .doc(userId)
+      .collection("userLikes")
+      .doc(postId)
+      .delete()
+      .then(() => decreaseLikesCount(postId, creatorId));
+  };
+
+  console.log("likee", likedPosts, posts);
 
   return (
     <View style={{ flex: 1 }}>
@@ -18,7 +76,7 @@ function Feed({ userFollowingPosts, navigation }) {
         horizontal={false}
         style={{ flex: 1 }}
         keyExtractor={(item) => item.id}
-        data={userFollowingPosts}
+        data={posts}
         renderItem={({ item }) => (
           <View>
             <Image
@@ -26,15 +84,29 @@ function Feed({ userFollowingPosts, navigation }) {
               source={{ uri: `${item?.downloadURL}` }}
             />
             <Text>{item.downloadURL}</Text>
-            <Text
-              onPress={() =>
-                navigation.navigate("Comment", {
-                  postId: item.id,
-                })
-              }
-            >
-              View comments...
-            </Text>
+            <View>
+              <Text
+                onPress={() =>
+                  navigation.navigate("Comment", {
+                    postId: item.id,
+                    userId,
+                  })
+                }
+              >
+                View comments...
+              </Text>
+              <Text
+                onPress={() =>
+                  likedPosts.find((likedPostId) => likedPostId === item.id)
+                    ? onDislikePress(item.id, item.creatorId)
+                    : onLikePress(item.id, item.creatorId)
+                }
+              >
+                {likedPosts.find((likedPostId) => likedPostId === item.id)
+                  ? "Dislike"
+                  : "Like"}
+              </Text>
+            </View>
           </View>
         )}
       />
@@ -42,9 +114,18 @@ function Feed({ userFollowingPosts, navigation }) {
   );
 }
 
+const styles = StyleSheet.create({
+  emptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
 const mapStateToProps = (store) => ({
   currentUser: store.userState.currentUser,
   userFollowingPosts: store.userState.userFollowingPosts,
+  likedPosts: store.userState.likedPosts,
 });
 
 // const mapDispatchToProps = (dispatch) =>
